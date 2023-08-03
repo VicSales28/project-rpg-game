1255bee080c00b47a9b559d693312e67a17d9371
Victoria Sales <contatovictoriasales@gmail.com>
2023-08-03 13:15:34 -0300

    Cria classes que herdam de Race

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  static _count = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._count += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc._count;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
